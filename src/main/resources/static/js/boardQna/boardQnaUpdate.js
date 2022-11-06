b52a81a49600cbda8033c5ec042529675604cecd
parkbico <jwpark5428@gmail.com>
2022-11-07 01:02:00 +0900

    # 파일업로드 만들기 진행중,,,

--- src/main/resources/static/js/boardQna/boardQnaUpdate.js
+++ src/main/resources/static/js/boardQna/boardQnaUpdate.js
@@ -9,6 +9,21 @@ var main = {
             _this.update();
         });
 
+        $('#files').on('change' , function(){
+            console.log("변화가 있었다.");
+            var fileInput =  $('input[name="files"]');
+
+            for( var i=0; i<fileInput.length; i++ ){
+                if( fileInput[i].files.length > 0 ){
+                    for( var j = 0; j < fileInput[i].files.length; j++ ){
+                        console.dir(fileInput[i].files[j].name); // 파일명 출력
+                        // 파일명 하단의 목록에 보여주게 하기
+                        // 목록에서 선택한 파일 없애기 ,, file 전역 변수로 만들어서 보관해뒀다가 삭제하면 리스트에서 제거
+                    }
+                }
+            }
+        });
+
 //        $('#btn-delete').on('click', function () {
 //            _this.delete();
 //        });
@@ -33,9 +48,16 @@ var main = {
 //            alert(JSON.stringify(error));
 //        });
 //    },
+
     update : function () {
 
         console.log(" 답변 내용 저장하기");
+
+        var files = $('input[name="files"]')[0].files;
+
+        for(var i = 0 ; i < files.length ; i++ ){
+            console.dir(files[i]);
+        }
         return false;
 //        var data = {
 //            title: $('#title').val(),
